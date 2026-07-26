ac7a56cc807a590c961a2f039a58e55c2ab6a0cf
Onlyadmirer <akmalrbc6@gmail.com>
2026-07-26 10:08:51 +0800

    feat: add google oauth login

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,4 +2,8 @@ export interface Users {
   id: number
   name: string
   email: string
+  is_verified?: boolean
+  oauth_id?: string | null
+  oauth_provider?: string | null
+  avatar_url?: string | null
 }
